ac7e6347a3d62db99f827004c2cf673ec464e921
NikitaBogoslovskiy <nbog@sfedu.ru>
2022-12-28 03:16:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def factorial(n: int):
+	if n == 0 or n == 1:
+		return 1
+	return n * factorial(n - 1)
+
+
+res = factorial(0)
+assert(res == 1)
+res = factorial(1)
+assert(res == 1)
+res = factorial(5)
+assert(res == 120)
+res = factorial(9)
+assert(res == 362880)
+print("Success!")
+
