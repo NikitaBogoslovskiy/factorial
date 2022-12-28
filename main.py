3bcc231ca5e07aff6fdaa7322c422dbeeac9ab40
NikitaBogoslovskiy <nbog@sfedu.ru>
2022-12-28 04:52:35 +0300

    correct main

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def factorial(n: int):
-	if n == 0 or n == 1:
-		return 1
-	return n * factorial(n - 1)
+from funcs import factorial
 
 
-# Делаем специальную синтаксическую ошибку
-print("Hello world!
 res = factorial(0)
 assert(res == 1)
 res = factorial(1)
