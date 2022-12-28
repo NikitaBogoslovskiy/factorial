5b0d2d9ed405364b5a4c86d3eb20da9eae4717bb
NikitaBogoslovskiy <nbog@sfedu.ru>
2022-12-28 06:02:58 +0300

    haha

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from funcs import factorial
 
 
-res = factorial(0)
-assert(res == 1)
-res = factorial(1)
-assert(res == 1)
-res = factorial(5)
-assert(res == 120)
-res = factorial(9)
-assert(res == 362880)
-print("Success!")
+res = factorial(4)
+print(f"Factorial of 4 = {res}")
 
