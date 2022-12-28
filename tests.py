a4eecbc7b44ee735dd6b473f9ad8030f878d9976
NikitaBogoslovskiy <nbog@sfedu.ru>
2022-12-28 05:13:16 +0300

    replace unittests with pytest

--- tests.py
+++ tests.py
@@ -1,23 +1,22 @@
 from funcs import factorial
-import unittest
+import pytest
 
 
-class TestFactorial(unittest.TestCase):
-    def test_0(self):
-        self.assertEqual(factorial(0), 1)
-
-    def test_1(self):
-        self.assertEqual(factorial(1), 1)
-
-    def test_5(self):
-        self.assertEqual(factorial(5), 120)
-
-    def test_8(self):
-        self.assertEqual(factorial(8), 40320)
-
-    def test_13(self):
-        self.assertEqual(factorial(13), 6227020800)
+def test_factorial_0():
+    assert factorial(0) == 1, "failed factorial(0)"
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_factorial_1():
+    assert factorial(1) == 1, "failed factorial(1)"
+
+
+def test_factorial_5():
+    assert factorial(5) == 120, "failed factorial(5)"
+
+
+def test_factorial_8():
+    assert factorial(8) == 40320, "failed factorial(8)"
+
+
+def test_factorial_13():
+    assert factorial(13) == 6227020800, "failed factorial(13)"
